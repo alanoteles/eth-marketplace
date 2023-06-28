049c57b339c9ea781d855f1b27b938ab0b220912
Alano Teles <alanoteles@gmail.com>
2023-06-28 18:01:03 -0300

    Added Course Card

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,5 +1,5 @@
 
-import { CourseList } from "@components/ui/course"
+import {CourseCard, CourseList} from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
 import { WalletBar } from "@components/ui/web3";
@@ -8,21 +8,23 @@ import {useNetwork} from "@components/hooks/web3/useNetwork";
 
 export default function Marketplace({courses}) {
   const { account } = useAccount()
-  const { network, networkList } = useNetwork()
-  console.log("net:", networkList)
+  const { network } = useNetwork()
   return (
     <>
-      {/*{console.log("net:", networkList)}*/}
       <div className="py-4 text-black">
         <WalletBar
           address={account.data}
           network={network.data}
-          networkList={ networkList }
         />
       </div>
-      <CourseList
-        courses={courses}
-      />
+      <CourseList courses={courses}>
+        {course =>
+          <CourseCard
+            key={course.id}
+            course={course}
+          />
+        }
+      </CourseList>
     </>
   )
 }
